818936d21cf4c4296cc7dfbe9e626cf9b13b799c
zhangll6 <zhangll6@ziroom>
2020-04-04 20:49:31 +0800

    创建项目

---
+++ src/main/java/com/example/redis/faceredis/FaceRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis.faceredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redis/faceredis/FaceRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis.faceredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
